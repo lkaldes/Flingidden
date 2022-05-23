194b8ef565129c7f6b04258fcaaee61edab1bfcc
lkaldes <lkaldes@ucsc.edu>
2022-05-22 17:13:04 -0700

    comments

--- src/scenes/Level0.js
+++ src/scenes/Level0.js
@@ -15,7 +15,7 @@ class Level0 extends Phaser.Scene {
 
     create(){
 
-        //movement
+        //movement and scene creation
         this.add.tileSprite(0, 0, 720, 860, 'title').setOrigin(0, 0);
         this.obstacle1 = this.physics.add.sprite(100, 250, 'obstacle').setScale(4).setSize(136, 40).setOffset(-18, 81);
         this.obstacle1.angle = 90;
@@ -24,6 +24,7 @@ class Level0 extends Phaser.Scene {
         this.obstacle1.body.immovable = true;
         this.obstacle2.body.immovable = true;
         this.playerturn = 0;
+        // flip a coin to determine starting position
         if (Phaser.Math.Between(1,2) == 1) {
             this.player = this.physics.add.sprite(720/2, 10, 'circle').setSize(30, 30).setOrigin(.5);
             this.arrow = this.physics.add.sprite(720/2, 430, 'arrowp2').setSize(30, 30).setOrigin(-.31,.45);
@@ -33,10 +34,12 @@ class Level0 extends Phaser.Scene {
             this.playerturn++;
         }
 
+        // create goals
         this.goal1 = this.physics.add.sprite(85, 85, 'goal').setScale(0.75).setSize(30, 30);
         this.goal2 = this.physics.add.sprite(635, 785, 'goal').setScale(0.75).setSize(30, 30);
         this.graphics = this.add.graphics();
         
+        // ball/arrow properties
         this.player.setGravityY(0);
         this.player.body.allowRotation = true;
         this.player.body.debugShowVelocity = true;
@@ -44,50 +47,61 @@ class Level0 extends Phaser.Scene {
         this.player.setFriction(2);
         this.slopey = 0.0;
         this.slopex = 0.0;
-        this.player.body.maxVelocity.setTo(1500, 1500);
         this.player.depth = 100;
         this.arrow.depth = 90;
 
-        
+        // movement properties (change for balance)
+        this.player.body.maxVelocity.setTo(1500, 1500);
+        this.gravity = 800;
+        this.drag = 500;
+
         //collision
         this.player.body.setCollideWorldBounds(true);
         this.physics.add.collider(this.player, this.obstacle1);
         this.physics.add.collider(this.player, this.obstacle2);
         this.physics.add.overlap(this.player, this.goal1, this.nextlevel, null, this);
         this.physics.add.overlap(this.player, this.goal2, this.nextlevel, null, this);
+        // mouse functions
         this.input.on('pointerup', this.fling.bind(this));
         this.input.on('pointermove', this.point, this);
     }
 
     update(){
+        // show/hide arrow whether ball is moving or not
         if (this.player.body.velocity.x == 0 && Math.abs(this.player.body.velocity.y) < 5) {
             this.arrow.alpha = 100;
         } else {
             this.graphics.clear();
             this.arrow.alpha = 0;
         }
+        // change arrow color based on player turn
         if (this.playerturn % 2 == 0) {
             this.arrow.setTexture('arrowp1');
         } else {
             this.arrow.setTexture('arrowp2');
         }
+        // set offset of arrow
         this.arrow.body.position.x = this.player.body.position.x + 98;
         this.arrow.body.position.y = this.player.body.position.y + 3;
+        // set gravity of ball based on side of screen
         if (this.player.body.position.y < 430) {
-            this.player.setGravityY(-800);
+            this.player.setGravityY(-this.gravity);
         } else if (this.player.body.position.y > 430){
-            this.player.setGravityY(800);
+            this.player.setGravityY(this.gravity);
         }
+        // bounce sound
         if ((this.player.body.blocked.down || this.player.body.blocked.left || this.player.body.blocked.right || this.player.body.blocked.up) && (this.player.body.velocity.x != 0 && Math.abs(this.player.body.velocity.y) >= 5)) {
             this.sound.play('bounce');
         }
+        // prevent sliding when touching surface
         if (this.player.body.blocked.down || this.player.body.blocked.up) {
-            this.player.setDragX(500);
+            this.player.setDragX(this.drag);
         } else {
             this.player.setDragX(0);
         }
     }
 
+    // launch mechanics chen clicked
     fling(pointer, player) {
         if (this.player.body.velocity.x == 0 && Math.abs(this.player.body.velocity.y) < 5) {
             this.graphics.clear();
@@ -99,6 +113,7 @@ class Level0 extends Phaser.Scene {
         }
     }
 
+    // arrow pointing when mouse moves
     point(pointer, player) {
         if (this.player.body.velocity.x == 0 && Math.abs(this.player.body.velocity.y) < 5) {
             this.graphics.clear();

182ffb88c5bfef56605322b530268ec6c2f3cd34
lkaldes <lkaldes@ucsc.edu>
2022-06-02 01:07:16 -0700

    blur BG and no debug

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ let config = {
             //debug: true
         },
         matter: {
-            debug: true
+            //debug: true
         }
     },
     scene: [ Menu, LevelSelect1, LevelSelect2, LevelSelect3, LevelSelect4, Level0, Level1, Level2, Level3, Level4, Level5, Level6, Level7, Level8, Level9, 
